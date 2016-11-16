0d295878f1ef13d3fd2a1dcf2f32d5334e8817ad
EddyMurphy <Rnbteam@hotmail.com>
2016-11-16 15:11:10 +0100

    Ajoutez des fichiers projet.

---
+++ Test/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+
+        static void WBash(ref string s)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            string s = "Hello World !";
+            char[] chartab = s.ToCharArray();
+            char[] newtab = new char[chartab.Length - 1];
+            int j = 0;
+            for (int i = 0; i < chartab.Length; i++)
+            {
+                if (chartab[i] == 'W')
+                    continue;
+                newtab[j] = chartab[i];
+                j++;
+            }
+            s = new string(newtab);
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
